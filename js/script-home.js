9ee786be69710e0197422f674bea9d69156e7734
tika-woww <145320307+tika-woww@users.noreply.github.com>
2025-03-16 19:56:17 +0800

    Add files via upload

---
+++ js/script-home.js
@@ -0,0 +1,16 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+
+    closeAllSubMenus()
+}
+
+function closeAllSubMenus(){
+    Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+        ul.classList.remove('show')
+        ul.previousElementSibling.classList.remove()
+    })
+}
